fe94628fc7322f8b57b85acc3615ce2007ac2719
Danish Salmani <salmanidanish488@gmail.com>
2024-08-09 22:18:48 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { NavBar } from "./components/NavBar"
+import './index.css';
+
+function App() {
+  
+  return (
+    <div>
+      <NavBar />
+      </div>  
+     
+  )
+}
+
+export default App
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,28 @@
+export const NavBar = () => {
+    return (
+        <div className="mx-12 mt-4">
+        <div className="flex justify-between items-center ">
+            <div>
+            <img src="/logo.svg" alt="" width={150} />
+            </div>
+
+            <div className="hidden md:flex flex-row space-x-8 ">
+                <div>Customers</div>
+                <div>Features</div>
+                <div>Integrations</div>
+                <div>Pricing</div>
+            </div>
+
+            <div className="hidden md:flex flex-row space-x-8">
+                <div>Sign in</div>
+                <div>Sign up</div>
+            </div>
+
+            <div className="block md:hidden">
+                <img src="menu.svg" alt="" className="" />
+            </div>
+
+            </div>
+        </div>
+    )
+}  
