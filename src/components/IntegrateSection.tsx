314777d9e05a5e652203bb472f01af66299d925f
Danish Salmani <salmanidanish488@gmail.com>
2024-08-13 02:07:33 +0530

    Integration section Completed

--- src/components/IntegrateSection.tsx
+++ src/components/IntegrateSection.tsx
@@ -30,6 +30,36 @@ export const IntegrateSection = () => {
             <div className="mt-12 underline text-center cursor" >
             ✨ See all 100+ integrations
             </div>
+
+            <div>
+                <Card />
+            </div>
+
+            <div>
+            <div className="mt-36 flex justify-center">
+            <div className="text-4xl md:text-5xl md:w-7/12 font-bold text-center ">
+            Ready to collect testimonials?
+            </div>
+            </div>
+            <div className="mt-6 md:flex justify-center">
+            <div className="text-xl text-center text-slate-300 md:w-8/12">
+            We are loved by Fortune 500 companies, early-stage startups, marketing agencies, real estate agents, freelancers, and many more. Your customers' testimonials are the best social proof you can get! Get started now 👇
+            </div></div>
+            </div>
+
+            <div className="mt-6 flex space-x-12 justify-center">
+                <div>No coding skills required</div>
+                <div>Start in under 2 minutes</div>
+            </div>
+
+            <div className="mt-6 flex space-x-12 justify-center ">
+                <div><button className="border">Get started with FREE Credits</button></div>
+                <div><button className="border">talk to us</button></div>
+            </div>
+
+            <div className="mt-6 underline text-center">
+            See our pricing →
+            </div>
         </div>
     )
 }
@@ -39,6 +69,34 @@ const ImgCard = ({image}:any) => {
         <div>
             <div className="bg-white rounded-lg w-fit flex items-center h-16 px-4 py-2 md:px-12 md:py-4">
                 <img src={image} alt="" width={150} />
+            </div>
+        </div>
+    )
+}
+
+const Card = () => {
+    return (
+        <div>
+            <div className="mt-56 text-black md:mx-20 bg-yellow-50 w-auto h-auto rounded-lg">
+                <div className="pt-16 text-2xl font-bold px-4 md:px-12">
+                We embedded Testimonial.to on the last page of our Prehireforms (candidates' skills assessment forms) and candidates' testimonials started coming in automatically! <span className="bg-yellow-100">Testimonials collection is now automated and we don't need to ask customers or candidates to drop us testimonials anymore!</span>
+                </div>
+
+                <div className="flex items-center pb-16">
+                    <div className="mt-6 px-6">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/testimonialto.appspot.com/o/testimonials%2F63357365-386c-4ba9-aed5-f4e77ba3a228%2Favatar?alt=media&token=49e40757-1c90-4a48-bf0f-7a25dc85c4d4" alt="" className="rounded-full" width={80} />
+                    </div>
+                    <div className="text-xl">
+                        <div className="font-bold">
+                        Kam Ling
+                    </div>
+                    <div>
+                        Co-Founder at Prehireforms.com
+                    </div>
+                    </div>
+                    
+                </div>
+
             </div>
         </div>
     )
